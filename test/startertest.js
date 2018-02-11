c41508b59f6e7d69f3d824aa38a2c7de4e2388d7
Ted Anyansi <tanyansi@gmail.com>
2018-02-10 17:34:33 -0800

    added basic test coverage with mocha and chai

---
+++ test/startertest.js
@@ -0,0 +1,41 @@
+const chai = require('chai');
+const expect = require('chai').expect;
+const should = chai.should();
+const axios = require('axios');
+const chaiHttp = require('chai-http');
+const Promise = require('bluebird');
+
+chai.use(chaiHttp);
+
+describe('Server', function() {
+
+  it('Server works', function (done) {
+
+    axios('http://localhost:3000')
+    .then( (data) => {
+      console.log('REQUEST DATA', data);
+      expect(data.data).to.equal('Hello World!');
+      expect(data.status).to.equal(200);
+      done();
+    });
+  });
+
+  it('Get a Single movie Id works', function (done) {
+
+    chai.request('http://localhost:3000')
+      .get('/getmovie')
+      .query({id: 1399})
+      .then(function (res) {
+        console.log('RESPONSE OBJECT', typeof res.body);
+        expect(res).to.have.status(200);
+        expect(res.body[0]).to.have.property('movie_id');
+        expect(res.body[0]).to.have.property('title');
+        done();
+      })
+      .catch(function (err) {
+        throw err;
+      })
+
+  });
+
+})
